db53e6d38dcbe8848ed78dc22678274404e68712
pal <koolabhipal@gmail.com>
2016-07-28 00:33:34 +0530

    Help page additions

--- src/main/com/urlshortner/controller/BaseController.java
+++ src/main/com/urlshortner/controller/BaseController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class BaseController {
 
     public static final String HELP_TEXT = "Use /account for creating an account (POST data must contain 'accountId' in the request JSON)<br>" +
-            "Use /register for registering url's (POST data must contain 'url' and 'redirectType' in the request JSON). The request header must have the authentication header comprising of Basic auth (Basic 'base64Credentials')<br>"+
+            "Use /register for registering url's (POST data must contain 'url' in the request JSON). The request header must have the authentication header comprising of Basic auth (Basic 'base64Credentials')<br>"+
             "Examples :-<br>"+
             "curl -H \"Content-Type: application/json\" -X POST -d '{\"accountId\":\"sampleAccountId\"}' http://localhost:8080/account<br>"+
-            "curl -H \"Content-Type: application/json\" -H \"Authorization:Basic cG9wOmNvRGo5eg==\" -X POST -d '{\"url\":\"http://google.com\",\"redirectType\":\"301\"}' http://localhost:8080/register";
+            "curl -H \"Content-Type: application/json\" -H \"Authorization:Basic cG9wOmNvRGo5eg==\" -X POST -d '{\"url\":\"http://google.com\",\"redirectType\":\"301\"}' http://localhost:8080/register<br>"+
+            "Further more statistics of a specific user's url redirection can be obtained by using /statistic/{accountId}. The request header must have the authentication header comprising of Basic auth (Basic 'base64Credentials')<br>";
 
 
     @RequestMapping("/")
